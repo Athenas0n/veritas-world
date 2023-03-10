1e2390fdeaddb2d1534b003353c4fc9ed61936cc
Archie Einstein <lukemccullough1701@outlook.com>
2023-03-10 14:50:59 +0000

    homepage setup

---
+++ scripts/headerFade.js
@@ -0,0 +1,51 @@
+// Global Variables
+var transitionTimer;
+
+// header scrolling effect
+$(window).on('scroll', function(){
+    if($(window).scrollTop()){
+        $('header').addClass('hideHeader');
+        disableSideFade();
+        transitionTimer = setTimeout(disableSideDisplay, 600)
+    }
+    else{
+        $("header").removeClass("hideHeader");
+    }    
+})
+
+function showSideHeader() {
+    const sideHeader = document.querySelector(".sideHeader");
+        clearTimeout(transitionTimer);
+        if ((sideHeader.classList.contains("showSideHeader")) || (sideHeader.classList.contains("sideHeaderTransition"))){
+            disableSideFade();
+            transitionTime = setTimeout(disableSideDisplay,600);
+        } else {
+            enableSideDisplay();
+            transitionTimer = setTimeout(enableSideFade);
+        }  
+}
+
+function enableSideFade() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.add("sideHeaderTransition");
+}
+function enableSideDisplay() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.add("showSideHeader");
+}
+function disableSideFade() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.remove("sideHeaderTransition");
+}
+function disableSideDisplay() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.remove("showSideHeader");
+}
+
+
+window.onload = () => {
+    const burger = document.querySelector("#burgerLogo");
+    const changeFont = document.querySelector("#changeFont");
+    const changeFontSide = document.querySelector("#changeFontSide");
+    burger.addEventListener("click", showSideHeader);
+}
